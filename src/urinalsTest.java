c774f848c1375eb64e3e53fa32dec08605281072
vishnutejareddy <12vis7@gmail.com>
2022-10-27 17:43:16 -0700

    Added a unit test that fails

---
+++ src/urinalsTest.java
@@ -0,0 +1,15 @@
+package src;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class urinalsTest {
+    urinals uri;
+
+    @Test
+    void demoTestMethod(){
+        System.out.println("=====Vishnu Vantukala == TEST ONE EXECUTED=====");
+        assertTrue(false);
+    }
+}
